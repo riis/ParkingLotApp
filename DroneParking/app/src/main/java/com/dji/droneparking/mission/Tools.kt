a4b4cacb0c54f6b85a37f5802ff1883be6e0e7d2
Wahid Bawa <wahidbawa10@gmail.com>
2021-07-21 12:57:11 -0400

    initial commit

---
+++ DroneParking/app/src/main/java/com/dji/droneparking/mission/Tools.kt
@@ -0,0 +1,26 @@
+package com.dji.droneparking.mission
+
+import android.app.Activity
+import android.widget.Toast
+
+object Tools {
+    fun getInt(str: String?, defaultValue: Int): Int {
+        return try {
+            Integer.valueOf(str)
+        } catch (e: Exception) {
+            defaultValue
+        }
+    }
+
+    fun getFloat(str: String?, defaultValue: Float): Float {
+        return try {
+            java.lang.Float.valueOf(str)
+        } catch (e: Exception) {
+            defaultValue
+        }
+    }
+
+    fun showToast(activity: Activity, msg: String?) {
+        activity.runOnUiThread { Toast.makeText(activity, msg, Toast.LENGTH_SHORT).show() }
+    }
+}
