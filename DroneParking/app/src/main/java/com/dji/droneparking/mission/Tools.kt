8943d2ab4bb0f7b29d039f4014370c9b65266f1c
Wahid Bawa <wahidbawa10@gmail.com>
2021-07-21 13:58:18 -0400

    got rid of annoying error in AS

--- DroneParking/app/src/main/java/com/dji/droneparking/mission/Tools.kt
+++ DroneParking/app/src/main/java/com/dji/droneparking/mission/Tools.kt
@@ -6,20 +6,18 @@ import android.widget.Toast
 object Tools {
     fun getInt(str: String?, defaultValue: Int): Int {
         return try {
-            Integer.valueOf(str)
+            Integer.valueOf(str!!)
         } catch (e: Exception) {
             defaultValue
         }
     }
-
     fun getFloat(str: String?, defaultValue: Float): Float {
         return try {
-            java.lang.Float.valueOf(str)
+            java.lang.Float.valueOf(str!!)
         } catch (e: Exception) {
             defaultValue
         }
     }
-
     fun showToast(activity: Activity, msg: String?) {
         activity.runOnUiThread { Toast.makeText(activity, msg, Toast.LENGTH_SHORT).show() }
     }
