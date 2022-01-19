bdae1c403491b08f005e41f518185b4a6595ea2b
daichow <daiansask@gmail.com>
2022-01-19 14:26:07 -0500

    made connecting to the drone a heck of a lot faster

--- app/src/main/java/com/dji/droneparking/core/ConnectionFragment.kt
+++ app/src/main/java/com/dji/droneparking/core/ConnectionFragment.kt
@@ -2,17 +2,22 @@ package com.dji.droneparking.core
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.dji.droneparking.R
-import com.dji.droneparking.viewmodel.ConnectionViewModel
+import dji.common.error.DJIError
+import dji.common.error.DJISDKError
+import dji.sdk.base.BaseComponent
+import dji.sdk.base.BaseProduct
+import dji.sdk.sdkmanager.DJISDKInitEvent
 import dji.sdk.sdkmanager.DJISDKManager
 import kotlinx.coroutines.*
 
@@ -27,9 +32,6 @@ class ConnectionFragment : Fragment() {
     private lateinit var mVersionTv: TextView
     private lateinit var animFadeIn: Animation
     private lateinit var animFadeOut: Animation
-
-    //viewModel used for SDK registration, and listening to product connectivity and hardware changes
-    private val model: ConnectionViewModel by viewModels()
 
     companion object {
         const val TAG = "ConnectionActivity"
@@ -60,92 +62,117 @@ class ConnectionFragment : Fragment() {
         mVersionTv.text =
             resources.getString(R.string.sdk_version, DJISDKManager.getInstance().sdkVersion)
 
-        initUI()
-        model.registerApp()
-        observers()
+        registerApp()
     }
 
-    //Function used to start displaying the dynamic UI elements
-    private fun initUI() {
+    //Function used to register the app with DJI's mobile SDK
+    private fun registerApp() {
         val tutorialFragment = TutorialFragment()
+        var rcConnection = false
+        var droneConnection = false
 
-        //To bypass this fragment and access the rest of the app without connecting to the DJI products, uncomment the line below:
-        //activity?.supportFragmentManager?.beginTransaction()?.replace(R.id.frameLayoutFragment, tutorialFragment, "tutorial")?.commit()
+        //Do this as soon as app starts
+        lifecycleScope.launch {
+            mTextConnectionStatus.setTextColor(Color.BLACK)
+            mTextConnectionStatus.text = "Connecting to RC"
+            while(!rcConnection){
+                mTextConnectionStatus.startAnimation(animFadeOut)
+                delay(2000)
+                mTextConnectionStatus.startAnimation(animFadeIn)
+                delay(2000)
+            }
 
-        //Coroutine used to display animations until the DJI drone and RC controller are connected
-        lifecycleScope.launch(Dispatchers.IO) {
+            while(!droneConnection){
+                mTextConnectionStatus.startAnimation(animFadeOut)
+                delay(2000)
+                mTextConnectionStatus.startAnimation(animFadeIn)
+                delay(2000)
+            }
+        }
 
-            //Looping until the DJI RC controller is connected to the user's mobile device
-            while (true) {
-                if (!model.rCConnected) {
-                    withContext(Dispatchers.Main) {
-                        mTextConnectionStatus.startAnimation(animFadeOut)
-                        delay(2000)
-                        mTextConnectionStatus.startAnimation(animFadeIn)
-                        delay(2000)
+        //NOTE: After installation, the app connects to the DJI server via internet and verifies the API key (in AndroidManifest.xml).
+        //Subsequent app starts will use locally cached verification information to register the app when the cached information is still valid.
+        DJISDKManager.getInstance()
+            .registerApp(context, object : DJISDKManager.SDKManagerCallback {
+
+                //Logging the success or failure of the registration
+                override fun onRegister(error: DJIError?) {
+                    if (error == DJISDKError.REGISTRATION_SUCCESS) {
+                        Toast.makeText(context, "Registration is Successful", Toast.LENGTH_SHORT).show()
+                        Log.i(TAG, "onRegister: Registration Successful")
+                    } else {
+                        Log.i(
+                            TAG,
+                            "onRegister: Registration Failed - ${error?.description}"
+                        )
+                        Toast.makeText(context, "Registration Failed", Toast.LENGTH_SHORT).show()
                     }
-                } else {
-                    //When connected, break loop and update UI
-                    withContext(Dispatchers.Main) {
+
+                }
+
+                //Called when the "product" is disconnected
+                override fun onProductDisconnect() {
+                    Log.i(TAG, "onProductDisconnect: Product Disconnected")
+                    Toast.makeText(context, "The RC is Disconnected", Toast.LENGTH_SHORT).show()
+                }
+
+                //Called when the "product" is connected
+                override fun onProductConnect(baseProduct: BaseProduct?) {
+                    Log.i(TAG, "onProductConnect: Product Connected")
+                    rcConnection = true
+                    //do this right after connecting to the RC
+                    lifecycleScope.launch {
                         mTextConnectionStatus.setTextColor(Color.GREEN)
                         mTextConnectionStatus.text = "Connected to RC ✓"
-                        delay(1000)
+                        delay(2000)
                         mTextConnectionStatus.setTextColor(Color.BLACK)
-                    }
-                    break
-                }
-            }
-
-            //Looping until the DJI drone is connected to its RC controller
-            while (true) {
-                if (!model.droneConnected) {
-                    withContext(Dispatchers.Main) {
                         mTextConnectionStatus.text = "Connecting to Drone"
-                        mTextConnectionStatus.startAnimation(animFadeOut)
-                        delay(2000)
-                        mTextConnectionStatus.startAnimation(animFadeIn)
-                        delay(2000)
                     }
-                } else {
-                    //When connected, break loop and update UI
-                    withContext(Dispatchers.Main) {
-                        mTextConnectionStatus.setTextColor(Color.GREEN)
-                        mTextConnectionStatus.text = "Connected to ${model.droneName}  ✓"
-                        delay(1000)
-                        mTextConnectionStatus.setTextColor(Color.BLACK)
-                    }
-                    break
                 }
-            }
 
-            //After both DJI products are connected, navigate to tutorialFragment.kt
-            activity?.supportFragmentManager?.beginTransaction()
-                ?.replace(R.id.frameLayoutFragment, tutorialFragment, "tutorial")?.commit()
-        }
-    }
+                //Called when the "product" is changed
+                override fun onProductChanged(baseProduct: BaseProduct?) {
+                    Log.i(TAG, "onProductChanged: Product Changed - $baseProduct")
 
-    //Function used to setup observers
-    private fun observers() {
-        //Listens to any connection changes to the rc controller and updates UI accordingly
-        model.connectionStatus.observe(viewLifecycleOwner, { isConnected ->
-            if (isConnected) {
-                mTextConnectionStatus.setTextColor(Color.GREEN)
-                mTextConnectionStatus.text = "Connected to RC ✓"
-                model.rCConnected = true
-            } else {
-                mTextConnectionStatus.text = "Connecting to RC"
-                model.rCConnected = false
-            }
-        })
+                    if(baseProduct?.model?.displayName != null){
+                        droneConnection = true
+                        lifecycleScope.launch {
+                            mTextConnectionStatus.setTextColor(Color.GREEN)
+                            mTextConnectionStatus.text = "Connected to ${baseProduct?.model?.displayName}  ✓"
+                            delay(2000)
+                        }
+                    }
+                }
 
-        //Listens to any connection changes to the drone and updates UI accordingly
-        model.product.observe(viewLifecycleOwner, { baseProduct ->
-            if (baseProduct != null && baseProduct.isConnected) {
-                model.droneConnected = true
-                model.droneName = baseProduct.model.displayName
-            } else {
-                mTextConnectionStatus.text = "Connecting to Drone"
-            }
-        })
+                //Called when a component object changes
+                override fun onComponentChange(
+                    componentKey: BaseProduct.ComponentKey?,
+                    oldComponent: BaseComponent?,
+                    newComponent: BaseComponent?
+                ) {
+                    Log.i(TAG, "onComponentChange key: $componentKey, oldComponent: $oldComponent, newComponent: $newComponent")
+                    newComponent?.let { component ->
+                        component.setComponentListener { connected ->
+                            Log.i(
+                                TAG,
+                                "onComponentConnectivityChange: $connected"
+                            )
+
+                            if (connected){
+                                //After both DJI products are connected, navigate to tutorialFragment.kt
+                                activity?.supportFragmentManager?.beginTransaction()
+                                    ?.replace(R.id.frameLayoutFragment, tutorialFragment, "tutorial")?.commit()
+                            }
+                        }
+                    }
+                }
+
+                override fun onInitProcess(p0: DJISDKInitEvent?, p1: Int) {}
+
+                override fun onDatabaseDownloadProgress(p0: Long, p1: Long) {}
+
+            })
+
+
     }
 }
